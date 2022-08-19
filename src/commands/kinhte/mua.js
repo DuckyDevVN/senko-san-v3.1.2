637a0def85e1f6e4d5cb33e7f714833f1f44ac3c
vitbuon345 <vitbuon2008@gmail.com>
2022-08-19 22:29:07 +0700

    kinhte

--- src/commands/kinhte/mua.js
+++ src/commands/kinhte/mua.js
@@ -8,58 +8,63 @@ const itemGo = [
   "thuốc độc",
   "thuốc giải độc",
   "pháo",
-  "khiên"
+  "khiên",
 ];
-const { MessageEmbed } = require('discord.js');
-const itemList = require('../../itemList');
-const config = require('../../config');
-const itemUser = require('../../Schemas/inventory');
+const { MessageEmbed } = require("discord.js");
+const itemList = require("../../itemList");
+const config = require("../../config");
+const itemUser = require("../../Schemas/inventory");
 
 module.exports = {
-  name: 'mua',
+  name: "mua",
   async execute(client, message, args) {
     let itemToBuy = itemGo[args[0]];
-    if (!itemToBuy) return message.channel.send({
-      content: `<@${message.author.id}>, bạn vui lòng chọn số thứ tự của item để chọn item để mua`
-    });
-    const varItem = !!itemList.find((value) => value.item.toLowerCase() === itemToBuy.toLowerCase());
-    if (!varItem) return message.channel.send({
-      content: `<@${message.author.id}>, mình không tìm thấy được item bạn chọn bạn vui lòng hãy kiểm tra lại!`
-    });
-    const itemGiatri = itemList.find((value) => value.item.toLowerCase() === itemToBuy.toLowerCase()).giatri;
+    if (!itemToBuy)
+      return message.channel.send({
+        content: `<@${message.author.id}>, bạn vui lòng chọn số thứ tự của item để chọn item để mua`,
+      });
+    const varItem = !!itemList.find(
+      (value) => value.item.toLowerCase() === itemToBuy.toLowerCase()
+    );
+    if (!varItem)
+      return message.channel.send({
+        content: `<@${message.author.id}>, mình không tìm thấy được item bạn chọn bạn vui lòng hãy kiểm tra lại!`,
+      });
+    const itemGiatri = itemList.find(
+      (value) => value.item.toLowerCase() === itemToBuy.toLowerCase()
+    ).giatri;
     const tienUser = await client.bal(message.author.id);
-    if (tienUser < itemGiatri) return message.channel.send(`Số tiền hiện tại của bạn hiện tại không đủ để mua item này`);
-    const emojiItem = itemList.find((value) => value.item.toLowerCase() === itemToBuy.toLowerCase()).emoji;
+    if (tienUser < itemGiatri)
+      return message.channel.send(
+        `Số tiền hiện tại của bạn hiện tại không đủ để mua item này`
+      );
+    const emojiItem = itemList.find(
+      (value) => value.item.toLowerCase() === itemToBuy.toLowerCase()
+    ).emoji;
     const embedMua = new MessageEmbed()
       .setTimestamp(Date.now())
       .setThumbnail(message.member.displayAvatarURL())
-      .setTitle(`${message.member.displayName}, bạn đã mua thành công item ${emojiItem + itemToBuy}!!`)
+      .setTitle(
+        `${message.member.displayName}, bạn đã mua thành công item ${
+          emojiItem + itemToBuy
+        }!!`
+      )
       .setColor(config.colorEmbed);
 
-    if (itemToBuy.toLowerCase() === "bento")
-      itemToBuy = "-1";
-     else if (itemToBuy.toLowerCase() === "bom")
-      itemToBuy = "-2";
-     else if (itemToBuy.toLowerCase() === "nhẫn")
-      itemToBuy = "-3";
-     else if (itemToBuy.toLowerCase() === "đơn ly hôn")
-      itemToBuy = "-4";
-     else if (itemToBuy.toLowerCase() === "chuối")
-      itemToBuy = "-5";
-     else if (itemToBuy.toLowerCase() === "chuối độc")
-      itemToBuy = "-6";
-     else if (itemToBuy.toLowerCase() === "thuốc độc")
-      itemToBuy = "-7";
-     else if (itemToBuy.toLowerCase() === "thốc giải độc")
-      itemToBuy = "-8";
-     else if (itemToBuy.toLowerCase() === "pháo")
-      itemToBuy = "-9";
-     else if (itemToBuy.toLowerCase() === "khiên")
-      itemToBuy = "-b1";
-    
+    if (itemToBuy.toLowerCase() === "bento") itemToBuy = "-1";
+    else if (itemToBuy.toLowerCase() === "bom") itemToBuy = "-2";
+    else if (itemToBuy.toLowerCase() === "nhẫn") itemToBuy = "-3";
+    else if (itemToBuy.toLowerCase() === "đơn ly hôn") itemToBuy = "-4";
+    else if (itemToBuy.toLowerCase() === "chuối") itemToBuy = "-5";
+    else if (itemToBuy.toLowerCase() === "chuối độc") itemToBuy = "-6";
+    else if (itemToBuy.toLowerCase() === "thuốc độc") itemToBuy = "-7";
+    else if (itemToBuy.toLowerCase() === "thốc giải độc") itemToBuy = "-8";
+    else if (itemToBuy.toLowerCase() === "pháo") itemToBuy = "-9";
+    else if (itemToBuy.toLowerCase() === "khiên") itemToBuy = "-b1";
+
     const param = {
-      User: message.author.id
-    }
+      User: message.author.id,
+    };
     itemUser.findOne(param, async (err, data) => {
       if (data) {
         const hasItem = Object.keys(data.Invemtory).includes(itemToBuy);
@@ -73,14 +78,14 @@ module.exports = {
         new itemUser({
           User: message.author.id,
           Invemtory: {
-            [itemToBuy]: 1
+            [itemToBuy]: 1,
           },
         }).save();
       }
       message.channel.send({
-        embeds: [embedMua]
-      })
-      client.rmv(message.author.id, itemGiatri)
+        embeds: [embedMua],
+      });
+      client.rmv(message.author.id, itemGiatri);
     });
-  }
-}
+  },
+};
